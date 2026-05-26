fcf841b3a1641e696114d0d67f369537fc28754e
Mohit Pithiya <mohitahir4124@gmail.com>
2026-05-26 23:49:54 +0530

    feat: implement document creation route and remove obsolete test routes

--- server/src/routes/document.js
+++ server/src/routes/document.js
@@ -1,2 +1,16 @@
+import express from "express";
 import User from "../models/User.js";
 import Document from "../models/Document.js";
+
+const router = express.Router();
+
+//create document
+router.post("/create" ,(req,res)=>{
+    try{
+    const {title,content,owner,collaborators} = req.body
+    }catch(error){
+        res.status(500).json({
+            message:error.message
+        })
+    }
+})
