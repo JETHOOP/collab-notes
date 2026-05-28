8f0a5477f2559867b7c882bc4c5fa83ba3678e5f
Mohit Pithiya <mohitahir4124@gmail.com>
2026-05-29 00:17:01 +0530

    refactor: add newline at end of user.js

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -35,5 +35,6 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.matchPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword , this.password)
 }
+
 const User = mongoose.model("User", userSchema);
 export default User;
